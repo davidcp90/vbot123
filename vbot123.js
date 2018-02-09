54b0f1facc3cce8ea150347f163cf36160ab35d1
David Felipe Camargo Polo <davidcp90@gmail.com>
2018-02-08 23:00:58 -0500

    vbot123 webhook

---
+++ vbot123.js
@@ -0,0 +1,14 @@
+'use strict';
+const BootBot = require('bootbot');
+const vBot = new BootBot({
+  accessToken: 'EAAD1qIeDj2sBAEZAbyBtRfTZCif9wnwdTC7PDah40rlq69DuLBpvxDPklbTNqCf3dupWaDyaKPZAF1pKqEqslUuZA6TMrbkzTcXE0PDgb9lhrSLaJzBXAzNYwBcmbdC9h5ETWxr2Tt1PU32oq2z6umhBMmRhREEWw08QGvRMFgZDZD',
+  verifyToken: 'fh7887987',
+  appSecret: 'adb9e46d32c6b561456022e89f2a632f'
+});
+
+vBot.on('message', (payload, chat) => {
+  const text = payload.message.text;
+  chat.say(`Hola amigos: ${text}`);
+});
+
+vBot.start(8081);
