08af09986b3b1a197c6a156153309d6f43cb0b61
David Felipe Camargo Polo <davidcp90@gmail.com>
2018-02-08 23:17:42 -0500

    test conversation

--- vbot123.js
+++ vbot123.js
@@ -6,9 +6,35 @@ const vBot = new BootBot({
   appSecret: 'adb9e46d32c6b561456022e89f2a632f'
 });
 
-vBot.on('message', (payload, chat) => {
-  const text = payload.message.text;
-  chat.say(`Hola amigos: ${text}`);
+bot.hear('ask me something', (payload, chat) => {
+	chat.conversation((convo) => {
+		askName(convo);
+	});
+
+	const askName = (convo) => {
+		convo.ask(`What's your name?`, (payload, convo) => {
+			const text = payload.message.text;
+			convo.set('name', text);
+			convo.say(`Oh, your name is ${text}`).then(() => askFavoriteFood(convo));
+		});
+	};
+
+	const askFavoriteFood = (convo) => {
+		convo.ask(`What's your favorite food?`, (payload, convo) => {
+			const text = payload.message.text;
+			convo.set('food', text);
+			convo.say(`Got it, your favorite food is ${text}`).then(() => sendSummary(convo));
+		});
+	};
+
+	const sendSummary = (convo) => {
+		convo.say(`Ok, here's what you told me about you:
+	      - Name: ${convo.get('name')}
+	      - Favorite Food: ${convo.get('food')}`);
+      convo.end();
+	};
 });
+
+
 
 vBot.start(8081);
