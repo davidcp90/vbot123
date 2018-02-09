99c3149c798fda78f3be9da6f29ee8aaac0566cc
David Felipe Camargo Polo <davidcp90@gmail.com>
2018-02-09 12:42:39 -0600

    greeting

--- vbot123.js
+++ vbot123.js
@@ -5,8 +5,11 @@ const vBot = new BootBot({
   verifyToken: 'fh7887987',
   appSecret: 'adb9e46d32c6b561456022e89f2a632f'
 });
-const greetingText = `Hello, {{user_first_name}}! What can I help you with?`;
-vBot.setGreetingText(greetingText);
+const greeting = {
+  locale: "default",
+  text: `Hello, {{user_first_name}}! What can I help you with?`
+};
+vBot.setGreetingText(greeting);
 /*vBot.on('message', (payload, chat) => {
 	const text = payload.message.text;
 	console.log(`The user said: ${text}`);
