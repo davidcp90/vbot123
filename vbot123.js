72d48cff95bcaf2ce32417c85bf028b3f84719b5
David Felipe Camargo Polo <davidcp90@gmail.com>
2018-02-08 23:24:48 -0500

    changed questions

--- vbot123.js
+++ vbot123.js
@@ -5,39 +5,37 @@ const vBot = new BootBot({
   verifyToken: 'fh7887987',
   appSecret: 'adb9e46d32c6b561456022e89f2a632f'
 });
-vBot.on('message', (payload, chat) => {
+/*vBot.on('message', (payload, chat) => {
 	const text = payload.message.text;
 	console.log(`The user said: ${text}`);
-});
+});*/
 vBot.hear('ask me something', (payload, chat) => {
 	chat.conversation((convo) => {
 		askName(convo);
 	});
-
-	const askName = (convo) => {
-		convo.ask(`What's your name?`, (payload, convo) => {
-			const text = payload.message.text;
-			convo.set('name', text);
-			convo.say(`Oh, your name is ${text}`).then(() => askFavoriteFood(convo));
-		});
-	};
-
-	const askFavoriteFood = (convo) => {
-		convo.ask(`What's your favorite food?`, (payload, convo) => {
-			const text = payload.message.text;
-			convo.set('food', text);
-			convo.say(`Got it, your favorite food is ${text}`).then(() => sendSummary(convo));
-		});
-	};
-
-	const sendSummary = (convo) => {
-		convo.say(`Ok, here's what you told me about you:
-	      - Name: ${convo.get('name')}
-	      - Favorite Food: ${convo.get('food')}`);
-      convo.end();
-	};
 });
+function askName(convo){
+  convo.ask(`What's your name?`, (payload, convo) => {
+    const text = payload.message.text;
+    convo.set('name', text);
+    convo.say(`Oh, your name is ${text}`).then(() => askFavoriteFood(convo));
+  });
+};
 
+function askFavoriteFood(convo){
+  convo.ask(`What's your favorite food?`, (payload, convo) => {
+    const text = payload.message.text;
+    convo.set('food', text);
+    convo.say(`Got it, your favorite food is ${text}`).then(() => sendSummary(convo));
+  });
+};
+
+function sendSummary(convo){
+  convo.say(`Ok, here's what you told me about you:
+      - Name: ${convo.get('name')}
+      - Favorite Food: ${convo.get('food')}`);
+    convo.end();
+};
 
 
 vBot.start(8081);
