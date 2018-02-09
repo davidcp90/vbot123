28f889b5a18301a3ce53ce02e1bcaa9d868de870
David Felipe Camargo Polo <davidcp90@gmail.com>
2018-02-08 23:00:54 -0500

    vbot123 webhook

--- config/fb.js
+++ config/fb.js
@@ -1,1 +1,5 @@
-access token EAAD1qIeDj2sBAFB79Av0zhGsQJphJW4IsoQZCUwwlyJ5jcbyrO7wHSsZBKFWTZAKdZBP9XuG3MHF4HgczzxO9nTmcyYoBRZBf0tnfClfJsEa33FHLS5ZC7wvRr2kLF7ZAaPA4XoEoRr8s41dZCeiZC4nYdJcKp10GSJjVjhmgpcPOCwZDZD
+export const fbConfig = {
+  accessToken: 'EAAD1qIeDj2sBAEZAbyBtRfTZCif9wnwdTC7PDah40rlq69DuLBpvxDPklbTNqCf3dupWaDyaKPZAF1pKqEqslUuZA6TMrbkzTcXE0PDgb9lhrSLaJzBXAzNYwBcmbdC9h5ETWxr2Tt1PU32oq2z6umhBMmRhREEWw08QGvRMFgZDZD',
+  verifyToken: 'fh7887987',
+  appSecret: 'adb9e46d32c6b561456022e89f2a632f'
+};
